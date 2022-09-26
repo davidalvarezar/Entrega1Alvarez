456db6390df5ec69a76c29012b6d5fa8c1196764
CristinaDelgado <cristinadelgado1788@gmail.com>
2022-09-26 00:24:24 -0300

    Modificaciones a templates

--- App_Tienda/urls.py
+++ App_Tienda/urls.py
@@ -4,8 +4,10 @@ from App_Tienda.views import*
 
 urlpatterns = [
     path("", inicio, name = "inicio"),
-    path('presentacion/', presentacion, name = "presentacion"),
-    path('sucursales/', sucursales, name = "sucursales"),
-    path('buscar/', buscar, name = "buscar"),
-    path('productosa/', productosa, name= "productosa" ),
+    path('SobreNosotros/', SobreNosotros, name = "SobreNosotros"),
+    path('NuestrasOficinas/', NuestrasOficinas, name = "NuestrasOficinas"),
+    path('servicios/', servicios, name = "servicios"),
+    path('registrarse/', registrarse, name = "registrarse"),
+    path('contacto/', contacto, name = "contacto"),
+    
 ]
--- App_Tienda/views.py
+++ App_Tienda/views.py
@@ -7,19 +7,35 @@ def inicio(request):
     return render(request, "App_Tienda/inicio.html" )
 
 
-def presentacion(request):
+'''def presentacion(request):
     if request.method == 'POST':
         cliente= clientes(nombre = request.POST["nombre"], apellido = request.POST["apellido"], email= request.POST["email"])
         cliente.save()
         return render(request, "App_Tienda/presentacion.html")
-    return render(request, "App_Tienda/presentacion.html")
+    return render(request, "App_Tienda/presentacion.html")'''
+
+def SobreNosotros(request):
+    return render (request, "App_Tienda/SobreNosotros.html")
 
 
-def sucursales(request):
-    return render(request, "App_Tienda/sucursales.html")
+
+def NuestrasOficinas(request):
+    return render(request, "App_Tienda/NuestrasOficinas.html")
 
 
-def buscar(request):
+def servicios(request):
+    return render(request, "App_Tienda/servicios.html")
+
+
+def registrarse(request):
+    return render(request, "App_Tienda/registarse.html")
+    
+
+def contacto(request):
+    return render (request, "App_Tienda/contacto.html")
+
+
+'''def buscar(request):
     if request.method == "POST":
         nombre = request.POST["nombre"]
         if productos.objects.filter(nombre__contains=nombre):
@@ -27,12 +43,12 @@ def buscar(request):
             return render(request,"App_Tienda/buscar.html",{"productos":producto})
         else:    
             return HttpResponse("material no encontrado")
-    return render(request, "App_Tienda/buscar.html")
+    return render(request, "App_Tienda/buscar.html")'''
 
-def productosa(request):
+'''def productosa(request):
     if request.method == 'POST':
         producto= productos(nombre = request.POST["nombre"], categoria = request.POST["categoria"], precio= request.POST["precio"])
         producto.save()
         return render(request, "App_Tienda/productosa.html")
-    return render(request, "App_Tienda/productosa.html")   
+    return render(request, "App_Tienda/productosa.html")'''   
 
